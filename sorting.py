fc3ee62f0b3b8485c66a416a0a3f71dfc474783f
RomanTutskyi <romachikkj@gmail.com>
2022-11-30 18:16:57 +0200

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,37 +1,147 @@
-import os 
-import re 
+import os
 import sys
 from pathlib import Path
+import re
+import time
 
+EXTENTIONS = {
+    'images': ('.jpeg', '.png', '.jpg', '.svg', '.dng'),
+    'video': ('.avi', '.mp4', '.mov', '.mkv'),
+    'documents': ('.doc', '.docx', '.txt', '.pdf', '.xls', '.xlsx', '.pptx', '.djvu', '.rtf'),
+    'audio': ('.mp3', '.ogg', '.wav', '.amr'),
+    'archives': ('.zip', '.gz', '.tar'),
+}
+
+CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
+
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
+TRANS = {}
 
 main_folder: Path | None = None
 
-setings = input('You wanna delete the empty folders? Answer yes or no:   ')
-def check_folders():
+def check_path():
+    
+    global main_folder
+
     if len(sys.argv) < 2:
         print('Enter path to folder which should be cleaned')
         exit()
-    true_folder = len(sys.argv[2])
-    main_folder = true_folder 
 
+    true_folder = Path(sys.argv[1])
+    
 
     if (not true_folder.exists()) or (not true_folder.is_dir()):
         print('Path incorrect')
         exit()
+
+    main_folder = true_folder
+    translate()
     check_files(true_folder)
-def check_files(folder : Path):
+
+def check_files(folder: Path):
     for file in folder.iterdir():
-        if file.is_file:
+
+        if file.is_file():
+            
+            
             sort(file)
-        
-        if file.is_dir:
+            
+            
+            
+
+        if file.is_dir():
+            
             check_files(file)
+            
 
             if not any(file.iterdir()):
                 file.rmdir()
-                print("|{}".format("-"*50))
                 print(f'{file} deleting')
-def sort():
-    pass
+
+def sort(file: Path):
+    path = Path(file)
+    file_suffix = file.suffix.lower()
+    
+    file_name = file.stem
+    zero_station = 0
+     
+    for key, values in EXTENTIONS.items():
+        if file_suffix in values:
+            zero_station = key 
+    if zero_station != 0: 
+        norm_name = normalize(file_name)
+        new_file_name = norm_name + file_suffix
+        
+        end_folder = main_folder.joinpath(zero_station)
+
+        
+        end_folder.mkdir(exist_ok=True)
+        new_file_path = end_folder.joinpath(new_file_name) 
+        try:
+            file.rename(new_file_path)
+        except FileExistsError:
+            time_stamp = time.time()
+            new_file_path = end_folder.joinpath(norm_name + '_' + str(time_stamp) + file_suffix)
+            file.rename(new_file_path)
+    else:
+        norm_name = normalize(file_name)
+        new_file_name = norm_name + file_suffix
+        
+        end_folder = main_folder.joinpath('another')
+
+        
+        end_folder.mkdir(exist_ok=True)
+        new_file_path = end_folder.joinpath(new_file_name) 
+        try:
+            file.rename(new_file_path)
+        except FileExistsError:
+            time_stamp = time.time()
+            new_file_path = end_folder.joinpath(norm_name + '_' + str(time_stamp) + file_suffix)
+            file.rename(new_file_path)
+
+def translate():
+    for cyril, latin in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+        TRANS[ord(cyril)] = latin
+        TRANS[ord(cyril.upper())] = latin.upper()
+
+def normalize(file_name: str) -> str:
+    result = ""
+    
+    name =  file_name.split('.')
+    global ext 
+    ext = name.pop(-1)
+    name = '_'.join(name)
+    name = name.lower()
+    new_name = file_name.translate(TRANS)
+    
+    
+    some = re.findall(r'[a-zA-Z0-9]', new_name)
+    
+    for littera in name:
+        if littera not in some:
+            result += '_'
+            continue 
+        else:
+            result += littera
+    result =  result + '.' + ext 
+    namedir = os.path.dirname(file_name)
+    new_filepath = os.path.join(namedir, result)
+    
+    
+    if str(file_name) == str(new_filepath):
+        print("| No need changes")
+        print("|{}".format("-"*50))
+    
+    for symvol in new_name:
+        if not symvol.isdigit() and not symvol.isalpha() and symvol != '_':
+            new_name = new_name.replace(symvol, '_')
+    print(result,'Name have normal name')
+    return new_name
+
 if __name__ == '__main__':
-    check_folders()
+    
+    check_path()
+    print("|{}".format("*"*50))
+    print("|Done!! |")
+    print("|{}".format("*"*50))
